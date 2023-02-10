73b550c7009d8047be96a6a65677ad620fa0b521
Markus Dethlefsen <git@markus-dethlefsen.de>
2023-02-10 15:17:06 +0100

    use icon fron lib | change highlight color

--- packages/components/src/lib/icons/pathList.ts
+++ packages/components/src/lib/icons/pathList.ts
@@ -4,7 +4,11 @@ interface PathList {
 
 const pathList: PathList = {
   extern: ['M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6m4-3h6v6m-11 5L21 3'],
-  home: ['m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z', 'M9 22V12h6v10']
+  home: ['m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z', 'M9 22V12h6v10'],
+  disc: [
+    'M12 22C6.475 22 2 17.525 2 12S6.475 2 12 2s10 4.475 10 10-4.475 10-10 10z',
+    'M12 15a3 3 0 1 1 0-6 3 3 0 1 1 0 6z'
+  ]
 };
 
 export default pathList;
