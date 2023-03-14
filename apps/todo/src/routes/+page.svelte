61b484d91e6faec1323e9adbee8e3aedf3d7c66f
Markus Dethlefsen <git@markus-dethlefsen.de>
2023-03-14 15:07:53 +0100

    login now always redirects to /todo

--- apps/todo/src/routes/+page.svelte
+++ apps/todo/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <section>
   <h1>Login</h1>
 
-  <form method="POST" action="/login">
+  <form method="POST">
     <input type="text" name="username" placeholder="Username" />
     <input type="password" name="password" placeholder="Password" />
     <button type="submit">Login</button>
