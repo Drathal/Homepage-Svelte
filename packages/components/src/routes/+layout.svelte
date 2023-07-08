3aabd150ff3a6f2dcb628192ea20a4530f5fc7fa
Markus Dethlefsen <git@markus-dethlefsen.de>
2023-07-08 02:36:16 +0200

    some layout for components overview

--- packages/components/src/routes/+layout.svelte
+++ packages/components/src/routes/+layout.svelte
@@ -8,9 +8,29 @@
 
     <ul>
       <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
         <a href="/nav">Nav</a>
       </li>
     </ul>
   </aside>
-  <slot />
+  <section><slot /></section>
 </main>
+
+<style lang="scss">
+  aside {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 200px;
+    padding: 1rem;
+    overflow-y: auto;
+  }
+
+  section {
+    margin-left: 200px;
+    padding: 0.5rem;
+  }
+</style>
